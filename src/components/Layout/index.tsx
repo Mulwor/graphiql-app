b715ee9531506b87d6ae7ceb93943018b2bc1aee
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2023-05-05 23:22:23 +0300

    refactor: change naming components

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom'
 
 import { Footer } from '../Footer'
-import { Header } from '../header/Header'
+import { Header } from '../Header'
 
 export const Layout = () => {
   return (
