13a084684c8b79629560f3f691ea49b2e788374e
Yulia <yudemins@mail.ru>
2023-05-06 01:18:56 +0300

    refactor: refactor comments

--- src/pages/graphipage/GraphiPage.tsx
+++ src/pages/graphipage/GraphiPage.tsx
@@ -3,12 +3,10 @@ import { buildClientSchema, printSchema } from 'graphql'
 import { useGetSchemaQuery } from '@/store'
 
 export const GraphiPage = () => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const { data, isFetching } = useGetSchemaQuery({})
 
   if (isFetching) return <div>isFetching</div>
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   console.log(data && printSchema(buildClientSchema(data)))
 
   return <div>Graphi Hello</div>
