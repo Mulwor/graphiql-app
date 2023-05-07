1988b530deed3de1de70a0ad3325254f23eb6f1b
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2023-05-07 23:02:02 +0300

    refactor: remove function that do not use

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,6 @@
-import { auth, db, logout } from '@root/src/firebase.config'
+import { auth, logout } from '@root/src/firebase.config'
 import { themeActions, useActionCreators, useAppSelector } from '@root/src/store'
-import { collection, getDocs, query, where } from 'firebase/firestore'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { NavLink, useNavigate } from 'react-router-dom'
 
@@ -9,7 +8,6 @@ import { Nav, Toggle } from '@/components'
 
 export const Header = () => {
   const [user, loading] = useAuthState(auth)
-  const [name, setName] = useState('')
   const navigate = useNavigate()
 
   const isDark = useAppSelector((state) => state.theme.isDark)
@@ -25,20 +23,6 @@ export const Header = () => {
 
   const handlerDark = () => {
     actions.setIsDark({ isDark: !isDark })
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const fetchUserName = async () => {
-    try {
-      const q = query(collection(db, 'users'), where('uid', '==', user?.uid))
-      const doc = await getDocs(q)
-      const data = doc.docs[0].data()
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-      setName(data.name)
-    } catch (error_) {
-      console.error(error_)
-      alert('An error occured while fetching user data')
-    }
   }
 
   useEffect(() => {
@@ -64,8 +48,7 @@ export const Header = () => {
               >
                 Main page
               </NavLink>
-              <div>{name}</div>
-              <div>{user?.email}</div>
+              <div>{user.email}</div>
               <button
                 className='login-button button-hover ml-5 bg-mainblue text-center text-white dark:bg-lightblue dark:text-darkblue'
                 onClick={logout}
