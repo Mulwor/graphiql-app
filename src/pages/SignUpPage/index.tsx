7c2bd0e1a57532194efc9d5850c52940b7d0a4c3
Yulia <yudemins@mail.ru>
2023-05-12 01:10:11 +0300

    fix: fix mistakes

--- src/pages/SignUpPage/index.tsx
+++ src/pages/SignUpPage/index.tsx
@@ -22,7 +22,7 @@ export const SignUp = () => {
   }, [user, loading, history])
 
   return (
-    <div className='flex h-full w-full max-w-7xl grow items-start justify-center  gap-7 '>
+    <div className='flex h-full w-full grow items-start justify-center gap-7'>
       <div className='flex h-full max-w-full shrink grow basis-0.5'>
         <AuthImage />
       </div>
@@ -36,8 +36,7 @@ export const SignUp = () => {
               type='text'
               className='bg-gray-50 border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 block w-full rounded-lg border p-2.5 text-sm dark:text-white'
               value={name}
-              // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-              onChange={(e: { target: { value: any } }) => setName(e.target.value)}
+              onChange={(e) => setName(e.target.value)}
               placeholder='Full Name'
             />
             <input
@@ -45,8 +44,7 @@ export const SignUp = () => {
               className='bg-gray-50 border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 mt-5 block w-full rounded-lg border p-2.5 text-sm dark:text-white'
               placeholder='johnIsFigos@company.com'
               value={email}
-              // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-              onChange={(e: { target: { value: any } }) => setEmail(e.target.value)}
+              onChange={(e) => setEmail(e.target.value)}
             />
             <input
               type='password'
