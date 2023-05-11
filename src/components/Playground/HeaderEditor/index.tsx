df78aa35019a87f6c026651d0459935dba4e698d
Kamran <sodapng@ya.ru>
2023-05-11 18:09:07 +0400

    feat: variables & headers

---
+++ src/components/Playground/HeaderEditor/index.tsx
@@ -0,0 +1,46 @@
+import { Prec } from '@codemirror/state'
+import { keymap } from '@codemirror/view'
+import { langs } from '@uiw/codemirror-extensions-langs'
+import { materialLight, materialLightInit } from '@uiw/codemirror-themes-all'
+import CodeMirror from '@uiw/react-codemirror'
+import cx from 'clsx'
+import { memo } from 'react'
+
+import { values } from '@/components/Playground'
+
+type HeaderEditorProps = {
+  value?: string
+  onKeyDown?: (event: React.KeyboardEvent<HTMLDivElement>) => void
+  className?: string
+}
+
+const extensions = [
+  materialLightInit(),
+  langs.json(),
+  Prec.high(
+    keymap.of([
+      {
+        key: 'Mod-Enter',
+        run: () => true,
+      },
+    ]),
+  ),
+]
+
+const handleChange = (value: string) => {
+  values.value.headers = value.trim()
+}
+
+export const HeaderEditor = memo(({ value, onKeyDown, className }: HeaderEditorProps) => {
+  return (
+    <CodeMirror
+      className={cx('h-full w-full border', className)}
+      height='100%'
+      theme={materialLight}
+      value={value}
+      onChange={handleChange}
+      onKeyDown={onKeyDown}
+      extensions={extensions}
+    />
+  )
+})
--- src/components/Playground/index.ts
+++ src/components/Playground/index.ts
@@ -1,2 +1,5 @@
+export * from '@/components/Playground/HeaderEditor'
+export * from '@/components/Playground/playgroundState'
 export * from '@/components/Playground/QueryEditor'
-export * from '@/components/Playground/Response'
+export * from '@/components/Playground/ResponseEditor'
+export * from '@/components/Playground/VariableEditor'
