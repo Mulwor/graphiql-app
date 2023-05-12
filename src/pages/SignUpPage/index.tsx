d2805d2d29046305cdc6bdb8b9398f77650e2460
Yulia <yudemins@mail.ru>
2023-05-12 17:52:22 +0300

    feat: add adaptive for signinpage an signuppage

--- src/pages/SignUpPage/index.tsx
+++ src/pages/SignUpPage/index.tsx
@@ -22,11 +22,11 @@ export const SignUp = () => {
   }, [user, loading, history])
 
   return (
-    <div className='flex h-full w-full grow items-start justify-center gap-7'>
-      <div className='flex h-full max-w-full shrink grow basis-0.5'>
+    <div className='mx-auto flex max-w-7xl shrink grow flex-col-reverse gap-7 sm:columns-2 sm:flex-row'>
+      <div className='mx-auto w-full max-w-md shrink grow lg:self-center'>
         <AuthImage />
       </div>
-      <div className='mt-10 flex w-full shrink grow basis-0.5 flex-col'>
+      <div className='mx-auto w-full max-w-lg px-5 sm:flex sm:flex-col sm:p-0 lg:mt-[10%]'>
         <h2 className='text-4xl font-bold uppercase text-mainred'>GraphiQL</h2>
         <div className='text-2xl'>Sign up</div>
 
@@ -54,7 +54,7 @@ export const SignUp = () => {
               onChange={(e) => setPassword(e.target.value)}
             />
             <button
-              className='button-hover mt-10 w-1/2 max-w-full justify-center rounded-full bg-mainblue p-3.5 text-white dark:bg-lightblue dark:text-darkblue'
+              className='button-hover mt-10 w-full max-w-full justify-center rounded-full bg-mainblue p-1.5 text-white dark:bg-lightblue dark:text-darkblue md:w-1/2'
               onClick={register}
             >
               Sign up
