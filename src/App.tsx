694ad3325f0681620e9b5284ab281871893a1a1a
Yulia <yudemins@mail.ru>
2023-05-22 19:00:28 +0300

    fix: resolve checks

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import { Layout, ProtectedRoutes } from '@/components'
 import { GraphiPage, HomePage, NotFound, SignInPage, SignUpPage } from '@/pages'
