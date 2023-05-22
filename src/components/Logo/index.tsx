27ddc64fd3fb436ba4964fd05ec97e01342ce214
Yulia <yudemins@mail.ru>
2023-05-22 17:00:07 +0300

    refactor: change style logo

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -2,9 +2,9 @@ import { ReactComponent as LogoIcon } from '@/assets/graphql.svg'
 
 export const Logo = () => {
   return (
-    <div className='flex items-center transition-all hover:fill-white hover:text-darknavy dark:hover:text-white'>
-      <LogoIcon className='hover-svg h-14 w-14 fill-seagreen p-2.5' />
-      <div className='py-2.5 uppercase hover:text-darknavy dark:hover:text-white'>GraphiQL</div>
+    <div className='flex items-center transition-all hover:fill-darknavy hover:text-darknavy dark:hover:fill-white dark:hover:text-white'>
+      <LogoIcon className='hover-svg h-14 w-14 fill-seagreen py-2.5' />
+      <div className='-ml-2 py-2.5 text-lg uppercase'>GraphiQL</div>
     </div>
   )
 }
